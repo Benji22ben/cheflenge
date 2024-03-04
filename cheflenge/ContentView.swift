8f5595726800dd78a4a859200ad73cb2e723b4c7
Benjamin Marques <benji22ben@gmail.com>
2024-03-04 21:57:58 +0100

    Initial Commit

---
+++ cheflenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cheflenge
+//
+//  Created by Benjamin MARQUES on 04/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cheflenge/cheflengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheflengeApp.swift
+//  cheflenge
+//
+//  Created by Benjamin MARQUES on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct cheflengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
