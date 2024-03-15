a4b6ac6d512de0b6f146f27b672214a713693856
Benjamin MARQUES <benji22ben@gmail.com>
2024-03-15 14:15:03 +0100

    feat: Added pictures handling and camera view

---
+++ cheflenge/Pictures/PhotoItemView.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoItemView.swift
+//  cheflenge
+//
+//  Created by Benjamin MARQUES on 15/03/2024.
+//
+/*
+ See the License.txt file for this sample’s licensing information.
+ */
+
+import Photos
+import SwiftUI
+
+struct PhotoItemView: View {
+    var asset: PhotoAsset
+    var cache: CachedImageManager?
+    var imageSize: CGSize
+
+    @State private var image: Image?
+    @State private var imageRequestID: PHImageRequestID?
+
+    var body: some View {
+        Group {
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                ProgressView()
+                    .scaleEffect(0.5)
+            }
+        }
+        .task {
+            guard image == nil, let cache = cache else { return }
+            imageRequestID = await cache.requestImage(for: asset, targetSize: imageSize) { result in
+                Task {
+                    if let result = result {
+                        self.image = result.image
+                    }
+                }
+            }
+        }
+    }
+}
