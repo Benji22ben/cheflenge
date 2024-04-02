3db2c24aea200d729c760487401f61f691a86461
Benjamin MARQUES BALULA <marques.benjaminb@gmail.com>
2024-04-02 08:38:52 +0200

    big fix

---
+++ cheflenge/Views/FrameView.swift
@@ -0,0 +1,95 @@
+/// Copyright (c) 2021 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+struct FrameView: View {
+    var image: CGImage?
+    var model: ContentViewModel
+
+    private let label = Text("Video feed")
+    private static let barHeightFactor = 0.15
+
+    var body: some View {
+        if let image = image {
+            GeometryReader { geometry in
+                Image(image, scale: 1.0, orientation: .upMirrored, label: label)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height,
+                        alignment: .center)
+                    .clipped()
+                    .overlay(alignment: .bottom) {
+                        buttonsView()
+                            .frame(height: geometry.size.height * Self.barHeightFactor)
+                            .background(.black.opacity(0.75))
+                    }
+            }
+        } else {
+            EmptyView()
+        }
+    }
+
+    private func buttonsView() -> some View {
+        HStack(alignment: .center) {
+            Spacer()
+            Button {
+                model.takePicture()
+            } label: {
+                Label {
+                    Text("Take Photo")
+                } icon: {
+                    ZStack {
+                        Circle()
+                            .strokeBorder(.white, lineWidth: 3)
+                            .frame(width: 62, height: 62)
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 50, height: 50)
+                    }
+                }
+            }
+            Spacer()
+        }
+        .buttonStyle(.plain)
+        .labelStyle(.iconOnly)
+        .padding()
+    }
+}
+
+struct FrameView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameView(image: nil, model: ContentViewModel())
+    }
+}
